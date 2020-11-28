27123e2e7f7f0897efe926f58cebb1582e7edd1a
[hitodeface] <[ari0703sophia@gmail.com]>
2020-11-28 21:50:34 +0900

    Initial Commit

---
+++ SwiftApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftApp
+//
+//  Created by 有村海星 on 2020/11/28.
+//  Copyright © 2020 Kaisei Arimura. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
