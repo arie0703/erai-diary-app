ec840921113dd5b99f3ba7c3bcca3b801e04a743
[hitodeface] <[ari0703sophia@gmail.com]>
2020-12-08 22:30:57 +0900

    rewardの設定

--- SwiftApp/EditUser.swift
+++ SwiftApp/EditUser.swift
@@ -10,22 +10,29 @@ import SwiftUI
 
 struct EditUser: View {
     @ObservedObject var user = UserProfile()
-    
     @Environment(\.presentationMode) var presentationMode
     var body: some View {
         NavigationView {
             Form {
-                Section(header: Text(user.name)) {
-                    TextField("ユーザー名", text: $user.name)
+                Section(header: Text("ユーザー名")) {
+                    TextField("", text: $user.name)
                     
                 }
                 
-                Section(header: Text("今日もお疲れ様！")){
+                Section(header: Text("目標")) {
+                    TextField("例: 一日一回投稿する！", text: $user.goal)
+                    
+                }
+                
+                Section{
                     Button(action: {
-                        
+                        UserDefaults.standard.set(self.user.name, forKey: "name")
+                        UserDefaults.standard.set(self.user.goal, forKey: "goal")
+                        userName = self.user.name
+                        userGoal = self.user.goal
                         self.presentationMode.wrappedValue.dismiss()
                     }) {
-                        Text("投稿！")
+                        Text("更新")
                     }
                 }
             
